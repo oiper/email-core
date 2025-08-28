c85dc1ff3c006fe61a3602049267a6c4786acc60
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-08-28 23:58:31 +0600

    Refactor schema imports and remove unused parse5 helper functions
    - Updated import path for `hasForbiddenTags` and `hasUnknownTags` to point to the new location in `../lib/parse5`.
    - Deleted the `parse5.ts` file along with its associated test file as they are no longer needed.

--- src/schema/common.ts
+++ src/schema/common.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
+import { hasForbiddenTags, hasUnknownTags } from '../lib/parse5'
 import { ALLOWED_TEXT_HTML_TAGS, FORBIDDEN_HTML_TAGS } from './config'
-import { hasForbiddenTags, hasUnknownTags } from './helpers/parse5'
 
 export const hexColorSchema = z.string().regex(/^#(?:[0-9a-fA-F]{3}){1,2}$/)
 
