608acb4e05b9a958d816a36a2e2c556e13eda320
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-09-05 16:29:13 +0600

    Enhance HeadingNode component by adding dynamic styling options for alignment, padding, and border properties. Update heading styles to include additional text formatting options and background color support for improved customization.

--- src/render/components/heading.tsx
+++ src/render/components/heading.tsx
@@ -1,7 +1,7 @@
 import { Column, Heading, Row } from '@react-email/components'
-
 import { TEmailNodeHeading } from '../../schema'
 import { TEditorComponentRenderWithContentProps, TRenderNodeProps } from '../types.t'
+import { sidePaddingToCombined } from '../utils'
 
 export function HeadingNode(props: TRenderNodeProps<TEmailNodeHeading>) {
   const { ControlEditorWrapper, node } = props
@@ -9,13 +9,61 @@ export function HeadingNode(props: TRenderNodeProps<TEmailNodeHeading>) {
   function RenderHeading({ children }: TEditorComponentRenderWithContentProps) {
     return (
       <Row>
-        <Column>
+        <Column
+          align={node.align}
+          style={{
+            padding: sidePaddingToCombined({
+              top: node.containerPaddingTop,
+              left: node.containerPaddingLeft,
+              right: node.containerPaddingRight,
+              bottom: node.containerPaddingBottom,
+            }),
+          }}
+        >
           <Heading
             as={node.as}
             style={{
-              margin: 0,
-
+              color: node.color,
               textAlign: node.textAlign,
+              fontSize: node.fontSize,
+              fontFamily: node.fontFamily,
+              fontWeight: node.fontWeight,
+              lineHeight: node.lineHeight,
+              fontStyle: node.fontStyle,
+              textTransform: node.textTransform,
+              letterSpacing: node.letterSpacing,
+              wordSpacing: node.wordSpacing,
+              whiteSpace: node.whiteSpace,
+              textDecoration: [
+                node.textOverline ? 'overline' : '',
+                node.textUnderline ? 'underline' : '',
+                node.textLineThrough ? 'line-through' : '',
+              ]
+                .filter(Boolean)
+                .join(' '),
+
+              width: node.width && `${node.width}%`,
+              backgroundColor: node.bgColor,
+
+              margin: '0',
+              padding: sidePaddingToCombined({
+                top: node.paddingTop,
+                left: node.paddingLeft,
+                right: node.paddingRight,
+                bottom: node.paddingBottom,
+              }),
+
+              borderStyle: node.borderStyle,
+              borderColor: node.borderColor,
+              borderTopWidth: node.borderTopWidth,
+              borderBottomWidth: node.borderBottomWidth,
+              borderLeftWidth: node.borderLeftWidth,
+              borderRightWidth: node.borderRightWidth,
+
+              borderTopLeftRadius: node.radiusTopLeft,
+              borderTopRightRadius: node.radiusTopRight,
+              borderBottomLeftRadius: node.radiusBottomLeft,
+              borderBottomRightRadius: node.radiusBottomRight,
             }}
             {...(children ? { children } : { dangerouslySetInnerHTML: { __html: node.content } })}
           />
